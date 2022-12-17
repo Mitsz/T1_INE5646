f31427e690edd91a03f9c79686ff63a24f0a4781
gabriela.r.lother <gabriela.r.lother@cloud.gabriela.r.lother.vms.ufsc.br>
2022-12-16 23:38:26 -0200

    saving account data to mongodb

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db')
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const connectDB = require("./config/db");
+
 // load config
-dotenv.config({path:'./config/config.env'})
+dotenv.config({ path: "./config/config.env" });
 
-connectDB()
+// Passport config
+require("./config/passport")(passport);
 
-const app = express()
+connectDB();
 
-const PORT = process.env.PORT || 5000
+const app = express();
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
+// Handlebars
+app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
